c2c92564c06f58c840977e329d76a83b96c302f8
Sergio Gabriel <sergio23300033@aluno.cesupa.br>
2024-06-26 14:35:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /*if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  } */
+
+  html.classList.toggle("light")
+}
